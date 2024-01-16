b41e0f9120a50f11bdc4c5607e8034e2666f8d92
Phil <philippe.teigne@gmail.com>
2024-01-15 21:03:32 -0500

    refactor getAllReservations to error log

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -90,23 +90,24 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return pool
-    .query(`
-      SELECT properties.*, reservations.id, reservations.start_date, avg(property_reviews.rating) as average_rating
-      FROM reservations
-      JOIN properties ON reservations.property_id = properties.id
-      JOIN property_reviews ON properties.id = property_reviews.property_id
-      WHERE reservations.guest_id = $1
-      GROUP BY properties.id, reservations.id
-      ORDER BY reservations.start_date
-      LIMIT $2;`, [guest_id, limit])
+
+  const queryString = `
+  SELECT properties.*, reservations.id, reservations.start_date, avg(property_reviews.rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`;
+
+  return query(queryString, [guest_id, limit])
     .then(result => {
       return result.rows;
     })
     .catch((err) => {
       return err.message;
     });
-  // return getAllProperties(null, 2);
 };
 
 /// Properties
